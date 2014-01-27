e54eec05ca290940d44b18cf3578283d594cca92
nitroglycerine33 <nitroglycerine33@gmail.com>
2014-01-27 13:02:38 -0800

    Initial VZW S4 device commit

---
+++ eclipse.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/samsung/jfltevzw/full_jfltevzw.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
+
+PRODUCT_NAME := eclipse_jfltevzw
+PRODUCT_DEVICE := jfltevzw
+
